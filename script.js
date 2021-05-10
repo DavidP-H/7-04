92d029cbf0cc6fed2815973e9535b0c5733b4906
DavidHart05 <>
2021-05-10 14:34:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let day
+let age
+
+document.getElementById('button').addEventListener('click', checker)
+
+function checker () {
+  day = document.getElementById('input1').value
+  age = document.getElementById('input2').value
+  age = parseInt(age)
+  if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21)) {
+    document.getElementById('result').innerHTML = 'You are eligible!'
+  } else {
+    document.getElementById('result').innerHTML = 'You are not eligible.'
+  }
+}
